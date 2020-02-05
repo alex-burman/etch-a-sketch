7f8368475df3d6b079f35ee73cf6f9cabc7fec4d
alex-burman <alexburman90@gmail.com>
2020-02-05 11:01:24 -0700

    added function that creates and returns a grid of divs with classes assigned to each element

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+let generateGrid = function(gridSize, gridId, rowClass, rowMemberClass) {
+    const grid = document.createElement('div');
+    grid.id = `${gridId}`;
+    for (let i = 0; i < gridSize; i++) {
+        let row = document.createElement('div');
+        row.className = `${rowClass}`;
+        for (let j = 0; j < gridSize; j++) {
+            let rowMember = document.createElement('div');
+            rowMember.className = `${rowMemberClass}`;
+            rowMember.style.backgroundColor = 'black';
+            rowMember.style.display = 'inline-block';
+            rowMember.style.width = '10px';
+            rowMember.style.height = '10px';
+            row.appendChild(rowMember);
+        }
+        grid.appendChild(row);
+    }
+
+    return grid;
+}
+
+document.querySelector('body').appendChild(generateGrid(4, 'grid', 'row', 'rowMember'));
