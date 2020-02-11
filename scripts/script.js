ccc59f79f2da3f95524369576d1f44735320ba16
alex-burman <alexburman90@gmail.com>
2020-02-11 16:54:05 -0700

    Version to be submitted to Odin Project as my solution

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,60 @@
+const getCheckedRadio = function (name) {
+    const radio = document.getElementsByName(name);
+    let checked;
+    radio.forEach((el) => {
+        if (el.checked) checked = el;
+    });
+
+    return checked;
+}
+
+const getRandomColor = function () {
+    let color = '';
+    for (let i = 0; i < 3; i++) {
+        let randomNumber = Math.floor(Math.random() * 255);
+        color += `${randomNumber}, `
+    }
+    return color;
+}
+
+const getTransparency = function (e) {
+    let colorProperty = e.target.style.backgroundColor;
+    let transparencyProperty = colorProperty.substring(
+        13,
+        colorProperty.length - 1
+    );
+    let currentTransparency = parseFloat(transparencyProperty);
+    return (
+        currentTransparency < 1
+        ? currentTransparency + 0.1
+        : 1
+    );
+}
+
+const fillGridItem = function (e) {
+    let radioSelection = getCheckedRadio('fill');
+    switch (radioSelection.value) {
+        case 'black':
+            e.target.style.setProperty('background-color', 'rgba(0, 0, 0, 1)');
+            break;
+        case 'color':
+            const randomColor = getRandomColor();
+            e.target.style.setProperty('background-color', `rgba(${randomColor}1)`);
+            break;
+        case 'fade':
+            const transparency = getTransparency(e);
+            e.target.style.setProperty(
+                'background-color', `rgba(0,0,0,${transparency})`
+            );
+            break;
+    }
+}
+
 const generateGridItem = function () {
     let gridItem = document.createElement('div');
 
     gridItem.classList = 'grid-item';
-    gridItem.style.backgroundColor = 'white';
-    gridItem.addEventListener('mouseover', () => 
-        gridItem.style.setProperty('background-color', '#222')
-    );
+    gridItem.style.backgroundColor = 'rgba(0, 0, 0, 0.0)';
     return gridItem;
 }
 
@@ -16,19 +65,31 @@ const generateGrid = function (gridItemQuantity) {
     grid.id = 'grid';
     grid.style.setProperty('--grid-cols', gridItemQuantity);
     grid.style.setProperty('--grid-rows', gridItemQuantity);
+    grid.addEventListener('mouseover', (e) => {
+        if (e.target.classList.value != 'grid-item') {
+            return;
+        }
+        fillGridItem(e);
+    });
 
+    const childFragment = document.createDocumentFragment();
     for (let i = 0; i < gridResolution; i++) {
-        grid.appendChild(generateGridItem());
+        childFragment.appendChild(generateGridItem());
     }
+    grid.appendChild(childFragment);
 
     return grid;
 }
 
-document.querySelector('#btn-reset').addEventListener('click', () => {
+document.querySelector('#btn-reset').addEventListener('mouseup', () => {
     let newGridNumber = NaN;
     while (isNaN(newGridNumber) == true) {
         newGridNumber = prompt('Enter Squares Per Side (max number = 100)');
-        newGridNumber = Number.parseInt(newGridNumber);
+        if (newGridNumber == null) {
+            return;
+        } else {
+            newGridNumber = Number.parseInt(newGridNumber);
+        }
     }
 
     if (newGridNumber > 100) {
@@ -39,8 +100,8 @@ document.querySelector('#btn-reset').addEventListener('click', () => {
 
     const newGrid = generateGrid(newGridNumber);
     document.getElementById('grid').remove();
-    document.querySelector('body').appendChild(newGrid);
+    document.querySelector('#sketch-grid-container').appendChild(newGrid);
 });
 
 const newGrid = generateGrid(16);
-document.querySelector('body').appendChild(newGrid);
+document.querySelector('#sketch-grid-container').appendChild(newGrid);
